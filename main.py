b535eda637ee24a9402ca5d4222c4be16d6a1f8b
ND <90370462+skepsl@users.noreply.github.com>
2022-10-24 20:07:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -17,19 +17,18 @@ class Main:
         dataset = CrossValidation(subject_id=self.arg.subject_id, fold_id=self.arg.k_fold)
         x_train, y_train, x_test, y_test = dataset.getData()
 
-        # x_train, y_train, x_test, y_test = x_train[:50], y_train[:50], x_test[:50], y_test[:50]
-
         self.arg.test_len = x_test.shape[0]
 
         graph = GraphRepresentation(self.arg)
         adj_train = graph.fit(x_train, y_train, rank=100)
         adj_test = graph.transform(x_test)
 
-        np.save('adj_train', adj_train)
-        np.save('adj_test', adj_test)
+        # Save for re-run later
+        np.save(f'adj_train_S{subject}_{fold}', adj_train)
+        np.save(f'adj_test_S{subject}_{fold}', adj_test)
 
-        adj_train = np.load('adj_train.npy')
-        adj_test = np.load('adj_test.npy')
+        adj_train = np.load('adj_train_S{subject}_{fold}'.npy')
+        adj_test = np.load('adj_test_S{subject}_{fold}.npy')
 
         train_iters = Iterator(x_train, y_train, adj_train)
         test_iters = Iterator(x_test, y_test, adj_test)
