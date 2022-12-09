9c1deb0c20addf0de51c97fe14da984cf5431206
ND <90370462+skepsl@users.noreply.github.com>
2022-12-09 15:29:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 import numpy as np
 
-from utils import Args, CrossValidation, Iterator, GetData
+from utils import Args, CrossValidation, Iterator, LoadData
 from trainers import Train
 from torch.utils.data import DataLoader
 from NMI import GraphRepresentation
 
 
 class Main:
-    def __init__(self):
-        self.arg = Args().get_args()
-        
+    def __init__(self, dataset):
+        self.arg = Args(dataset).get_args()
 
     def fit(self, subject, fold):
-        GetData(self.arg).getSubjectData(subject)
         self.arg.subject_id = subject
         self.arg.k_fold = fold
 
-        dataset = CrossValidation(subject_id=self.arg.subject_id, fold_id=self.arg.k_fold)
+        LoadData(self.arg).run()
+        dataset = CrossValidation(args=self.arg)
         x_train, y_train, x_test, y_test = dataset.getData()
 
         self.arg.test_len = x_test.shape[0]
 
         graph = GraphRepresentation(self.arg)
-        adj_train, edges = graph.fit(x_train, y_train, rank=100)
+        adj_train, edges = graph.fit(x_train, y_train, rank=200)
         adj_test = graph.transform(x_test)
 
-        np.save(f'adjacency/adj_train_{subject}_{fold}', adj_train)
-        np.save(f'adjacency/adj_test_{subject}_{fold}', adj_test)
-        np.save(f'adjacency/edges_{subject}_{fold}', edges)
-        adj_train = np.load(f'adjacency/adj_train_{subject}_{fold}.npy')
-        adj_test = np.load(f'adjacency/adj_test_{subject}_{fold}.npy')
+        np.save(f'adjacency_{self.arg.dataset}/adj_train_{subject}_{fold}', adj_train)
+        np.save(f'adjacency_{self.arg.dataset}/adj_test_{subject}_{fold}', adj_test)
+        np.save(f'adjacency_{self.arg.dataset}/edges_{subject}_{fold}', edges)
+
+        adj_train = np.load(f'adjacency_{self.arg.dataset}/adj_train_{subject}_{fold}.npy')
+        adj_test = np.load(f'adjacency_{self.arg.dataset}/adj_test_{subject}_{fold}.npy')
 
         train_iters = Iterator(x_train, y_train, adj_train)
         test_iters = Iterator(x_test, y_test, adj_test)
@@ -48,18 +48,5 @@ class Main:
 
 
 if __name__ == '__main__':
-
-    # Junhee
-    subjects = list(np.linspace(1, 10, 10, dtype=int))
-    
-    # Seunghoon
-    subjects = list(np.linspace(11, 32, 22, dtype=int))
-    
-    # Deny
-    subjects = list(np.linspace(33, 54, 22, dtype=int))
-    
-    
-    for subject in subjects:
-        for fold in range(5):
-            framework = Main()
-            framework.fit(subject=subject, fold=fold)
+    framework = Main('lee')
+    framework.fit(subject=1, fold=0)
